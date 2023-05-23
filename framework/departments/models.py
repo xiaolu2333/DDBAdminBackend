cbeb49842d580bb6091c9ba4dbb670cb0c3bbbf4
dangfulin <2694977226@qq.cmo>
2023-05-23 10:16:39 +0800

    机构、部门、用户模型调整。

--- framework/departments/models.py
+++ framework/departments/models.py
@@ -1,17 +1,14 @@
 from django.db import models
 
+from framework.organizations.models import Organization
+
 
 class Department(models.Model):
     name = models.CharField("部门名称", max_length=255)
     code = models.CharField("部门代码", max_length=255)
     parentCode = models.CharField("上级部门", max_length=255)
-    deptLevel = models.CharField("部门级别", max_length=255)
-    deptDesc = models.TextField("部门描述")
-    levelDesc = models.TextField("机构层级描述")
     enabled = models.BooleanField("启用")
-    sortNum = models.IntegerField("排序")
-    orgCode = models.CharField("所属机构", max_length=255)
-    remark = models.TextField("备注")
+    org = models.ForeignKey(Organization, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
