ef683a1537292a9519de42fcb8dab285a66288ed
dangfulin <2694977226@qq.cmo>
2023-05-29 16:56:16 +0800

    修改部门模型。

--- framework/departments/models.py
+++ framework/departments/models.py
@@ -4,11 +4,14 @@ from framework.organizations.models import Organization
 
 
 class Department(models.Model):
+    id = models.AutoField(primary_key=True)
     name = models.CharField("部门名称", max_length=255)
     code = models.CharField("部门代码", max_length=255)
     parentCode = models.CharField("上级部门", max_length=255)
     enabled = models.BooleanField("启用")
     org_id = models.ForeignKey(Organization, on_delete=models.CASCADE)
+    create_time = models.DateTimeField(auto_now_add=True)
+    update_time = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
