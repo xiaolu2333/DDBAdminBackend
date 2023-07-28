a01e507fee9354f39500cc8ff9b067253432bde0
dangfulin <2694977226@qq.cmo>
2023-07-28 10:21:44 +0800

    系统管理：调整菜单、角色、用户模型字段

--- framework/authentication/models.py
+++ framework/authentication/models.py
@@ -8,10 +8,18 @@ from framework.departments.models import Department
 class Role(models.Model):
     # 角色ID
     id = models.AutoField(primary_key=True)
+    # 角色类型
+    type = models.IntegerField(null=False)  # 1-管理角色 2-业务角色 3-普通角色
     # 角色名
     name = models.CharField(max_length=255)
+    # 角色编码
+    code = models.CharField(max_length=255)
+    # 是否启用
+    enabled = models.BooleanField(default=True)
+    # 排序
+    sort = models.IntegerField(default=0)
     # 角色描述
-    desc = models.CharField(max_length=255)
+    desc = models.TextField()
     # 创建时间
     create_time = models.DateTimeField(auto_now_add=True)
     # 更新时间
@@ -36,9 +44,13 @@ class User(models.Model):
     # 用户所属部门
     dept = models.ForeignKey(Department, on_delete=models.CASCADE)
     # 用户角色
-    role = models.ForeignKey(Role, on_delete=models.CASCADE)
+    role = models.ForeignKey(Role, on_delete=models.CASCADE, null=True, blank=True)
     # 是否启用
     enabled = models.BooleanField(default=True)
+    # 排序
+    sort = models.IntegerField(default=0)
+    # 用户描述
+    desc = models.TextField()
     # 创建时间
     create_time = models.DateTimeField(auto_now_add=True)
     # 更新时间
--- framework/resources/models.py
+++ framework/resources/models.py
@@ -11,17 +11,33 @@ class Menu(models.Model):
     url = models.CharField(max_length=32, null=False)
     # 父级菜单
     parent = models.ForeignKey('self', on_delete=models.CASCADE, null=False)
-    # 是否显示
-    is_show = models.BooleanField(default=True)
+    # 页面类型
+    page_type = models.IntegerField(null=False)  # 1-主目录/模块 2-路由
+    # 菜单类型
+    menu_type = models.IntegerField(null=False)  # 1-用户页面 2-系统管理 3-安全管理 4-审计管理 5-运维管理
+    # 授权类型
+    auth_type = models.IntegerField(null=False)  # 1-公开 2-角色授权 3-用户登录
+    # 路由路径
+    path = models.CharField(max_length=255, null=False)
+    # 页面路径
+    component_path = models.CharField(max_length=255, null=False)
+    # 图标
+    icon = models.CharField(max_length=255, null=False)
+    # 是否启用
+    enable = models.BooleanField(default=True)
     # 排序
     sort = models.IntegerField(default=0)
     # 菜单组件
     component = models.CharField(max_length=255, null=False)
     # 分配给角色
-    role = models.ForeignKey(Role, on_delete=models.CASCADE, related_name='role_menus', null=False)
+    role = models.ForeignKey(Role, on_delete=models.CASCADE, related_name='role_menus', null=True, blank=True)
+    # 创建人
+    create_user = models.CharField(max_length=32, null=False)
     # 创建时间
     create_time = models.DateTimeField(auto_now_add=True)
-    # 更新时间
+    # 修改人
+    update_user = models.CharField(max_length=32, null=False)
+    # 修改时间
     update_time = models.DateTimeField(auto_now=True)
 
     def __str__(self):
