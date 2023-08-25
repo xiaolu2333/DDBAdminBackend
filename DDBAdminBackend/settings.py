598eaa1b494ff5314c24bc5d1288311000892bff
dangfulin <2694977226@qq.cmo>
2023-08-25 08:25:15 +0800

    refactor:使用postgersql数据库；调整数据管理树节点模型

--- DDBAdminBackend/settings.py
+++ DDBAdminBackend/settings.py
@@ -86,20 +86,20 @@ WSGI_APPLICATION = 'DDBAdminBackend.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    # 使用 sqlite3 数据库
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-    # # 使用 postgresql 数据库
+    # # 使用 sqlite3 数据库
     # 'default': {
-    #     'ENGINE': 'django.db.backends.postgresql',
-    #     'NAME': 'DDBAdmin',
-    #     'USER': 'postgres',
-    #     'PASSWORD': 'postgres',
-    #     'HOST': '127.0.0.1',
-    #     'PORT': '5432',
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
     # }
+    # 使用 postgresql 数据库
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'DDBAdmin',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+    }
 }
 
 # Password validation
